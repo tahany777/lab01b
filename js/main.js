72e02e2cd6025f476ec18746e41dd1b187b2790f
tahany777 <tahanymohammed777@hotmail.com>
2021-06-06 19:02:19 +0300

    html css js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 let users = ["How do you feel about putting pineapple on pizza?", "What would you name your boat if you had one?",
 "What’s the most useless talent you have?", "If you were a fruit, what fruit would you be?"];
 let output = [];
-for(i = 0; i < users.length; i++){
+for(let i = 0; i < users.length; i++){
         let input = prompt(users[i]);
         output.push(input);
         alert(`${users[i]} ${input}`);
         console.log(`${users[i]} ${input}`);
 }
-alert(`" ${output} " `); 
+alert(`" ${output} "`); 
