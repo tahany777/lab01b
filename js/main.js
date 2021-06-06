59c1769b527f208bce33ffb807d2ffddb024ce1a
tahany777 <tahanymohammed777@hotmail.com>
2021-06-06 16:26:35 +0300

    html and css and js

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+        let questionOne = prompt("How do you feel about putting pineapple on pizza?");
+
+        alert(`${questionOne}`);
+
+        console.log(`How do you feel about putting pineapple on pizza? ${questionOne}`);
+
+        let questionTwo = prompt("What would you name your boat if you had one?");
+
+        alert(`${questionOne} , ${questionTwo}`);
+
+        console.log(`What would you name your boat if you had one? ${questionTwo}`);
+
+        let questionThree = prompt("What’s the most useless talent you have?");
+
+        alert(`${questionOne} , ${questionTwo} , ${questionThree}`);
+
+        console.log(`What’s the most useless talent you have? ${questionThree}`);
+
+        let questionFour = prompt("If you were a fruit, what fruit would you be?");
+
+        alert(`${questionOne} , ${questionTwo} , ${questionThree} , ${questionFour}`);
+        
+        console.log(`If you were a fruit, what fruit would you be? ${questionFour}`);
