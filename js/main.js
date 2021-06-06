26c4e83a724cc9cddb6fa8184408351dbfe53ff5
tahany777 <tahanymohammed777@hotmail.com>
2021-06-06 23:15:36 +0300

    html css js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
-let users = ["How Are you Doing?", "What would you name your boat if you had one?",
-"What’s the most useless talent you have?", "If you were a fruit, what fruit would you be?"];
-let output = [];
-for(let i = 0; i < users.length; i++){
-        let input = prompt(users[i]);
-        output.push(input);
-        alert(`" ${output} "`);
-        console.log(`${users[i]} ${input}`);
-}
+//question One
+let qOne = prompt("How Are you Doing?");
+alert(`" ${qOne} "`);
+console.log(`How Are you Doing ? ${qOne}`);
+
+//question Two
+let qTwo = prompt("What would you name your boat if you had one?");
+alert(`" ${qOne} , ${qTwo} "`);
+console.log(`What would you name your boat if you had one ? ${qTwo}`);
+
+//question Three
+let qThree = prompt("What’s the most useless talent you have?");
+alert(`" ${qOne} , ${qTwo} , ${qThree} "`);
+console.log(`What’s the most useless talent you have ? ${qThree}`);
+
+//question Four
+let qFour = prompt("If you were a fruit, what fruit would you be?");
+alert(`" ${qOne} , ${qTwo} , ${qThree} , ${qFour} "`);
+console.log(`If you were a fruit, what fruit would you be? ${qFour}`);
+
+
